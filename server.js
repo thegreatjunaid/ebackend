6998d454045265c2dcf6bcd6fe7db39530e00acc
thegreatjunaid <2023-3-60-432@std.ewubd.edu>
2026-05-30 13:16:18 +0600

    jj

--- server.js
+++ server.js
@@ -974,7 +974,7 @@ app.get("/auth/google/callback",
       "junaid_secret_key",         // same secret as your login
       { expiresIn: "1d" }          // same expiry as your login
     );
-    res.redirect(`http://localhost:5173/auth/success?token=${token}&userId=${req.user._id}`);
+    res.redirect(`https://thegreatjunaid.netlify.app/auth/success?token=${token}&userId=${req.user._id}`);
   }
 );
 
